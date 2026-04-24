19e3e7b92aab076e5bbc4f25e73738f7cc96b08e
Kamil Skowron <1019893+Cinderella-Man@users.noreply.github.com>
2026-04-24 19:18:08 +0200

    Intial application config

---
+++ lib/credence.ex
@@ -0,0 +1,18 @@
+defmodule Credence do
+  @moduledoc """
+  Documentation for `Credence`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Credence.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/credence_test.exs
@@ -0,0 +1,8 @@
+defmodule CredenceTest do
+  use ExUnit.Case
+  doctest Credence
+
+  test "greets the world" do
+    assert Credence.hello() == :world
+  end
+end
